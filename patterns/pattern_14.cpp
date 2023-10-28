91971c36d52601528e02445274a0d78ac6667369
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-28 15:24:48 +0600

    pattern 15 solved

--- patterns/pattern_14.cpp
+++ patterns/pattern_14.cpp
@@ -5,7 +5,7 @@ void pattern(int n)
     for (int i = 0; i < n; i++)
     {
         char ch = 'A';
-        for (int j = n - i; j > 0; j--)
+        for (int j = 0; j <= i; j++)
         {
             cout << ch++ << " ";
         }
