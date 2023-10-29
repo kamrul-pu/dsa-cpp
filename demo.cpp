c40f4bd151071ea32d111a9b88944668d3aca362
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-29 17:30:53 +0600

    fibo with recursion

--- demo.cpp
+++ demo.cpp
@@ -13,9 +13,24 @@ int fibo(int n)
     return dp[n] = fibo(n - 1) + fibo(n - 2);
 }
 
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    int a = 0, b = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return b;
+}
+
 int main()
 {
     int n;
     cin >> n;
     cout << fibo(n) << endl;
+    cout << fib(n) << endl;
 }
