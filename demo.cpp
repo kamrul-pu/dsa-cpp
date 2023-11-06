ef8466ed964911b1bf150e49811ae99e42d47243
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-06 23:13:36 +0600

    make element zero in 2d matrix

--- demo.cpp
+++ demo.cpp
@@ -2,96 +2,157 @@
 #include <vector>
 #include <unordered_set>
 using namespace std;
+const int N = 100;
+int a[N][N];
 
-void print(vector<int> &arr, int n)
+void print(int n, int m)
 {
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " ";
+        for (int j = 0; j < m; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << "\n";
+    cout << endl;
 }
 
-int longest_consecutive_sequence(vector<int> &a, int n)
+void markRow(int i, int m)
 {
-    int longest = 1;
+    for (int j = 0; j < m; j++)
+    {
+        a[i][j] = -1;
+    }
+}
+
+void markCol(int j, int n)
+{
     for (int i = 0; i < n; i++)
     {
-        int x = a[i];
-        int ct = 1;
-        x++;
-        for (int j = i + 1; j < n; j++)
+        a[i][j] = -1;
+    }
+}
+
+void markZeros(int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
         {
-            if (a[j] == x)
+            if (a[i][j] == -1)
             {
-                ct++;
-                x++;
+                a[i][j] = 0;
             }
         }
-        longest = max(longest, ct);
     }
-    return longest;
 }
 
-int longest_consecutive_sequence1(vector<int> &a, int n)
+void set_matrix_zeros(int n, int m)
 {
-    int longest = 1, last_smaller = INT_MIN, ct = 0;
     for (int i = 0; i < n; i++)
     {
-        if (a[i] - 1 == last_smaller)
+        for (int j = 0; j < m; j++)
         {
-            ct++;
-            last_smaller = a[i];
-        }
-        else if (a[i] != last_smaller)
-        {
-            ct = 1;
-            last_smaller = a[i];
-        }
-        longest = max(longest, ct);
-    }
-    return longest;
-}
-
-int longest_consecutive_sequence2(vector<int> &a, int n)
-{
-    if (n == 0)
-        return 0;
-    int longest = 1;
-    unordered_set<int> st;
-    // insert the unique element in the set
-    for (int i = 0; i < n; i++)
-    {
-        st.insert(a[i]);
-    }
-    for (auto it : st)
-    {
-        if (st.find(it - 1) == st.end())
-        {
-            int cnt = 1;
-            int x = it;
-            while (st.find(x + 1) != st.end())
+            if (a[i][j] == 0)
             {
-                x++;
-                cnt++;
+                markRow(i, m);
+                markCol(j, n);
             }
-            longest = max(longest, cnt);
         }
     }
-    return longest;
+    markZeros(n, m);
+}
+void mark_row_col(int row[], int col[], int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 0)
+            {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    // make the row and column zero
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (row[i] == 1 || col[j] == 1)
+            {
+                a[i][j] = 0;
+            }
+        }
+    }
 }
 
+void mark_zero_optimal(int n, int m)
+{
+    int col0 = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 0)
+            {
+                // mark the ith row
+                a[i][0] = 0;
+                // mark the jth col
+                if (j != 0)
+                    a[0][j] = 0;
+                else
+                    col0 = 0;
+            }
+        }
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (a[i][j] != 0)
+            {
+                // check for col & row
+                if (a[0][j] == 0 || a[i][0] == 0)
+                {
+                    a[i][j] = 0;
+                }
+            }
+        }
+    }
+    if (a[0][0] = 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            a[0][j];
+        }
+    }
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            a[i][0] = 0;
+        }
+    }
+}
 int main()
 {
-    int n;
-    cin >> n;
-    vector<int> a(n);
+    int n, m;
+    cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
     }
-    // sort(a.begin(), a.end());
-    print(a, n);
-    cout << longest_consecutive_sequence2(a, n) << endl;
+    int col[m] = {0}, row[n] = {0};
+    print(n, m);
+    // set_matrix_zeros(n, m);
+    // mark_row_col(row, col, n, m);
+    mark_zero_optimal(n, m);
+    print(n, m);
     return 0;
 }
